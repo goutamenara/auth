37827b46eb98c28bb10dff153cac89f66428d55b
Goutam Enara <goutam@pm.me>
2020-12-15 21:04:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config')
-const port = 3000
+const port = process.env.PORT || 3000;
 const passport = require('passport')
 
 const sqlite3 = require('sqlite3')
